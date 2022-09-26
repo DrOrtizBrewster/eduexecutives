77862b61d85b9aa440c38a9569807eb01858b62a
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-09-26 18:20:48 -0500

    add

--- app2.js
+++ app2.js
@@ -1,17 +1,10 @@
-let Yes = "Yes";
-let otherYes = "yes";
-let No = "No";
-let no = "no";
-
-let LearnMore = prompt("Would you like to learn more?");
-if (LearnMore =="Yes") {
-    document.write("Super, Dr. Ortiz-Brewster will call you."); 
-} else if (LearnMore.toLowerCase == "yes") {
-    document.write("Super, Dr. Ortiz-Brewster will call you soon.");
-if (LearnMore =="No") {
-        document.write("That is fine. Let us know when you are ready to discuss this opportunity."); 
-} else if (FavoriteTypeOfLearning.toLowerCase == "no") {
+function askQuestion(){
+    let LearnMore = prompt("Would you like to learn more?");
+    if (LearnMore.toLowerCase() == "yes") {
+        document.write("Super, Dr. Ortiz-Brewster will call you soon.");
+    } else if (FavoriteTypeOfLearning.toLowerCase() == "no") {
         document.write("That is fine. Let us know when you are ready to discuss this opportunity in the near future.");
-} else  {}
-    document.write("Let us know if we can be of assistance.");
-    
+    } else  {
+        document.write("Let us know if we can be of assistance.");
+    }
+}
