46486fb43a9068724b44a9d15c87b3bda88d7f61
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-09-25 19:32:48 -0500

    more jS

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-alert("Hello Classmates!");
-
-document.getElementById("button").onclick = function() {
-    document.getElementById("Yes").innerHTML = "Wonderful News! We will contact you.";
-    document.getElementById("button").style.display = "none";
-}
 prompt("What is your favorite way to learn - traditional or virtual?");
 let traditional = "Traditional";
 let otherTraditional = "traditional";
-
 let FavoriteTypeOfLearning = Traditional;
 
 if (FavoriteTypeOfLearning =="Traditional") {
@@ -16,7 +9,14 @@ if (FavoriteTypeOfLearning =="Traditional") {
     console.log("That is fantastic. We can help you create your new traditional school!!");
 } else  {
     console.log("Great, we are experts in virtual learning too.");
-    
+
 let userType = prompt("What is your favorite way to learn - traditional or virtual?");
 console.log(userType);
 var username = prompt();
+
+
+
+document.getElementById("button").onclick = function() {
+    document.getElementById("Yes").innerHTML = "Wonderful News! We will contact you.";
+    document.getElementById("button").style.display = "none";
+}
