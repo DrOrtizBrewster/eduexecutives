ddd26c72e7a7a704ef041a6e805a6728b57e68c9
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-09-26 12:40:30 -0500

    updated to new items

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 let traditional = "Traditional";
 let otherTraditional = "traditional";
+let virtual = "Virtual";
+let othervirtual = "virtual";
 
 let FavoriteTypeOfLearning = prompt("What is your favorite way to learn - traditional or virtual?");
 if (FavoriteTypeOfLearning =="Traditional") {
     document.write("That is fantastic. We can help you create your new traditional school!!"); 
 } else if (FavoriteTypeOfLearning.toLowerCase == "traditional") {
-    document.write("That is fantastic. We can help you create your new traditional school!!");
-} else  {
-    document.write("Great, we are experts in virtual learning too.");
+    document.write("That is fantastic. We can help you create your new brick-and-mortar school!!");
+if (FavoriteTypeOfLearning =="Virtual") {
+        document.write("That is fantastic. We can help you create your new virtual school!!"); 
+} else if (FavoriteTypeOfLearning.toLowerCase == "virtual") {
+        document.write("That is fantastic. We can help you create your new online school!!");
+} else  {}
+    document.write("Great, we are experts in all types of learning models.");
+    
