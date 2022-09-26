5a370260d15565fb02db39c17f32b14b67972682
DrOrtizBrewster <112881441+DrOrtizBrewster@users.noreply.github.com>
2022-09-26 08:32:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ let otherTraditional = "traditional";
 let FavoriteTypeOfLearning = Traditional;
 
 if (FavoriteTypeOfLearning =="Traditional") {
-    console.log("That is fantastic. We can help you create your new traditional school!!"); 
+    document.write("That is fantastic. We can help you create your new traditional school!!"); 
 } else if (FavoriteTypeOfLearning.toLowerCase == "traditional") {
-    console.log("That is fantastic. We can help you create your new traditional school!!");
+    document.write("That is fantastic. We can help you create your new traditional school!!");
 } else  {
-    console.log("Great, we are experts in virtual learning too.");
+    document.write("Great, we are experts in virtual learning too.");
 
 let userType = prompt("What is your favorite way to learn - traditional or virtual?");
-console.log(userType);
+document.write(userType);
 var username = prompt();
 
 
