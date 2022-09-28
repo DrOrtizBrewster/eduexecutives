f079a67f8c11d0e8023dd50f0caab56ae235b507
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-09-28 14:03:48 -0500

    new borders

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ let FavoriteTypeOfLearning = prompt("What is your favorite way to learn - tradit
 
 if (FavoriteTypeOfLearning.toLowerCase() == traditional) {
     alert("That is fantastic. We can help you create your new brick-and-mortar school!!");
-    document.write("That is fantastic. We can help you create your new brick-and-mortar school!!");
 } else if (FavoriteTypeOfLearning.toLowerCase() == virtual) {
     alert("That is fantastic. We can help you create your new virtual school!!");
-    document.write("That is fantastic. We can help you create your new virtual school!!"); 
-
 } else  {
     document.write("Great, we are experts in all types of learning models.");
 }
