4522a2830358b38594c21c3950f95f5d5f02fe7a
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-09-28 17:40:14 -0500

    new

--- app2.js
+++ app2.js
@@ -2,7 +2,7 @@ function askQuestion(){
     let LearnMore = prompt("Would you like to learn more?");
     if (LearnMore.toLowerCase() == "yes") {
         document.write("Super, Dr. Ortiz-Brewster will call you soon.");
-    } else if (FavoriteTypeOfLearning.toLowerCase() == "no") {
+    } else if (LearnMore.toLowerCase() == "no") {
         document.write("That is fine. Let us know when you are ready to discuss this opportunity in the near future.");
     } else  {
         document.write("Let us know if we can be of assistance.");
