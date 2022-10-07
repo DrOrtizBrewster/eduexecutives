28a4d6ea493cd84acfc5b431cd50f0f9b925f1cb
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-10-07 09:55:02 -0500

    update function

--- app3.js
+++ app3.js
@@ -1,7 +1,8 @@
-function askErrorQuestion(){
-    let virtualQuestion = prompt("How long has virtual learning been around? Pick a year between 1980 and 2000");
+ let virtualQuestion = prompt("How long has virtual learning been around? Pick either 1986 or 1990");
     if (virtualQuestion.toLowerCase() == "1986") {
-        document.write("You are brilliant!");
+        alert("You are brilliant!");
+    } else if (virtualQuestion.toLowerCase() == "1990") {
+        alert("Sorry, that is incorrect. Please try again.");
     } else  {
-        document.write("Sorry, that is incorrect. Try again, but this time pick a year between 1982 and 1986.");
+        alert("Sorry, that is incorrect.");
     }
