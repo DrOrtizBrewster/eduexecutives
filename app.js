fc6a20588eb918ccf3a853cc6f79316072bcddd7
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-10-08 19:51:04 -0500

    functions and pictures

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 function favLearning() {
+    
     let FavoriteTypeOfLearning = prompt("What is your favorite way to learn - traditional or virtual?");
-    if (FavoriteTypeOfLearning.toLowerCase() == traditional) {
+
+    if (FavoriteTypeOfLearning.toLowerCase() == 'traditional') {
         alert("That is fantastic. We can help you create your new brick-and-mortar school!!");
-    } else if (FavoriteTypeOfLearning.toLowerCase() == virtual) {
+    } else if (FavoriteTypeOfLearning.toLowerCase() == 'virtual') {
         alert("That is fantastic. We can help you create your new virtual school!!");
     } else  {
-        alert("Great, we are experts in all types of learning models.");
+        alert("We are experts in all types of learning models.");
     }
 } 
-function virtualQuestion(){
+function virtualOption(){
     let virtualQuestion = prompt("How long has virtual learning been around? Pick either 1986 or 1990.");
 
     if (virtualQuestion.toLowerCase() == "1986") {
@@ -24,26 +26,40 @@ function askTime(){
     let time = prompt('What time is it?');
     let message;
 
-    if(time <= 11){
-        message = 'Good Morning!';
-    } else if (time <= 24){
-        message = 'Good Afternoon!';
-    }else {
-        message = 'Time does not exist';
+    if (time <= 11){
+        alert('Good Morning!');
+    } else if (time <= 18){
+        alert('Good Afternoon!');
+    } else if(time <=24){
+        alert('Good Evening!');
+    } else {
+        alert('Time does not exist');
+        askTime();
     }
 }
 function askQuestion(){
     let LearnMore = prompt("Would you like to learn more?");
 
     if (LearnMore.toLowerCase() == "yes") {
-        document.write("Super, Dr. Ortiz-Brewster will call you soon.");
+        alert("Super, Dr. Ortiz-Brewster will call you soon.");
     } else if (LearnMore.toLowerCase() == "no") {
-        document.write("That is fine. Let us know when you are ready to discuss this opportunity in the near future.");
+        alert("That is fine. Let us know when you are ready to discuss this opportunity in the near future.");
     } else  {
-        document.write("Let us know if we can be of assistance.");
+        alert("Let us know if we can be of assistance.");
     }
 }
-favLearning();
-virtualQuestion();
-askTime();
-askQuestion();
+function showPic(){
+    const userNum = prompt("How many times do you wanna see a funny picture?");
+
+    for(let i = 0; i < userNum; i++){
+        document.write("<img src='download.jpg' style='width:300px'/>");
+    }
+}
+function quiz(){
+    favLearning();
+    virtualOption();
+    askTime();
+    askQuestion();
+    showPic();
+}
+quiz()
