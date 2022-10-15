cb8dc567a232fdc51c1d628e0325383e9dc07e69
Dr. Ortiz-Brewster WORK <ortizbrewster@gmail.com>
2022-10-14 20:43:16 -0500

    updated while loop

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ function favLearning() {
     }
 } 
 function virtualOption(){
-    let virtualQuestion = prompt("How long has virtual learning been around? Pick either 1986 or 1990.");
-
-    if (virtualQuestion.toLowerCase() == "1986") {
-        alert("You are brilliant!");
-    } else if (virtualQuestion.toLowerCase() == "1990") {
-        alert("Sorry, that is incorrect. Please try again.");
-    } else  {
-        alert("Sorry, that is incorrect.");
-        virtualQuestion();
+    let unanswered = true;
+    while (unanswered) {
+        let virtualQuestion = prompt("How long has virtual learning been around? Pick either 1986 or 1990.");
+        if (virtualQuestion.toLowerCase() == "1986") {
+            alert("You are brilliant!");
+            unanswered = false
+        } else if (virtualQuestion.toLowerCase() == "1990") {
+            alert("Sorry, that is incorrect. Please try again.");
+        } else  {
+            alert("Sorry, that is incorrect.");
+          //  virtualQuestion();
+        }
     }
 }
 function askTime(){
